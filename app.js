534ee71e0a0784d0a0e85a8ca7aec76cf937e676
Piazza Carla Malena <carlamalenapiazzamail.com>
2023-06-12 10:45:01 -0300

    addhelmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const app = express();
 
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(helmet());
 
 //routes
 app.use(require('./src/routes/tp.routes'))
