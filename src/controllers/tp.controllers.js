ef6beb15706a751a9d6503919b9df9dbef89980a
Piazza Carla Malena <carlamalenapiazzamail.com>
2023-06-12 10:34:54 -0300

    controllers

--- src/controllers/tp.controllers.js
+++ src/controllers/tp.controllers.js
@@ -1,1 +1,16 @@
 
+ctrl.postTp = (req, res)=>{
+    res.send('post')
+}
+ctrl.putTp = (req, res)=>{
+   res.send('put')
+}
+ctrl.deleteTp = (req, res)=>{
+   res.send('delete')
+}
+ctrl.getTp = (req, res)=>{
+   res.send('get')
+}
+
+module.exports = ctrl;
+
